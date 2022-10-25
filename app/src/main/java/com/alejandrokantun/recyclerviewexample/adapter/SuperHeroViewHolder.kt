2629b098aa0cd537be1badc4095d60b232c1cdba
Alejandro Kantun <alejandrokantuncih@gmail.com>
2022-10-25 01:37:15 -0500

    third version, R classic was replaced with binding and click on image an recyclerview was added

--- app/src/main/java/com/alejandrokantun/recyclerviewexample/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/alejandrokantun/recyclerviewexample/adapter/SuperHeroViewHolder.kt
@@ -1,24 +1,40 @@
 package com.alejandrokantun.recyclerviewexample.adapter
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.alejandrokantun.recyclerviewexample.R
 import com.alejandrokantun.recyclerviewexample.SuperHero
+import com.alejandrokantun.recyclerviewexample.databinding.ItemSuperheroBinding
 import com.bumptech.glide.Glide
 
-class SuperHeroViewHolder(view:View):RecyclerView.ViewHolder(view) {
-    val superHeroName = view.findViewById<TextView>(R.id.tvSuperHeroName)
-    val realName = view.findViewById<TextView>(R.id.tvRealName)
-    val publisher = view.findViewById<TextView>(R.id.tvPublisher)
-    val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
+class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val binding = ItemSuperheroBinding.bind(view)
+
+    //val superHeroName = view.findViewById<TextView>(R.id.tvSuperHeroName)
+    //val realName = view.findViewById<TextView>(R.id.tvRealName)
+    //val publisher = view.findViewById<TextView>(R.id.tvPublisher)
+    //val photo = view.findViewById<ImageView>(R.id.ivSuperHero)
 
 
-    fun render (superHeroModel: SuperHero){
-        superHeroName.text = superHeroModel.superHero
-        realName.text = superHeroModel.realName
-        publisher.text = superHeroModel.publisher
-        Glide.with(photo.context).load(superHeroModel.photo).into(photo)
+    fun render(superHeroModel: SuperHero) {
+        binding.tvSuperHeroName.text = superHeroModel.superHero
+        binding.tvRealName.text = superHeroModel.realName
+        binding.tvPublisher.text = superHeroModel.publisher
+        Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+        //to make functional the image when is click, you can add setOnClikListener
+        binding.ivSuperHero.setOnClickListener {
+            Toast.makeText(
+                binding.ivSuperHero.context,
+                "El nombre es ${superHeroModel.realName}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        itemView.setOnClickListener{
+            Toast.makeText(
+                binding.ivSuperHero.context,
+                "El super héroe es ${superHeroModel.superHero}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
